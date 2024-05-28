851d181ae2f4f4c739f44ce8ce5cfd50f917aac4
Rishi Satsangi <rishi.satsangi@gmail.com>
2024-05-28 17:50:32 +0100

    refactored and added base components

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Playlist() {
+    return <div>playlist</div>;
+}
+
+export default Playlist;
---
+++ src/components/Track/Track.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Track() {
+    return <div>track</div>;
+}
+
+export default Track;
